22a4fdd4c49ad2674b8cced352577a093920a759
bongjoo.seo <bongjoo.seo@samsung.com>
2016-12-22 18:25:34 +0900

    Applied ES6 Supported to process multiple commands at once
    Signed-off-by: bongjoo.seo <bongjoo.seo@samsung.com>

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-var FauxMo = require('fauxmojs');
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var port = process.env.PORT || 2002;
-var exec = require('child_process').exec;
+import FauxMo from 'fauxmojs'
+import express from 'express'
+import http from 'http'
+import path from 'path'
+import { exec } from 'child_process'
+
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 2002
 
 // Creates the website server on the port #
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
+  console.log('Server listening at port %d', port)
+})
 
 // configure Express
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'html')
 
 // Express Routing
-app.use(express.static(__dirname + '/public'));
-app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, '/public')))
+app.engine('html', require('ejs').renderFile)
 
-var sendCommand = function(command) {
-  console.log(command);
-  exec("irsend SEND_ONCE ollehtv " + command);
-};
+const sendCommand = (command) => {
+  console.log(command)
+  exec('irsend SEND_ONCE ollehtv ' + command)
+}
+
+const sleep = (ms = 0) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 // WEMO virtual device
-var fauxMo = new FauxMo({
+const fauxMo = new FauxMo({
   devices: [
     {
       name: 'tv',
       port: 11000,
-      handler: function(action) {
-        console.log('tv action:', action);
-        setTimeout(function() {
-          sendCommand("KEY_SETTOPPOWER");
-        }, 200);
+      handler: (action) => {
+        console.log('tv action:', action)
+        sendCommand('KEY_SETTOPPOWER')
       }
     }
   ]
-});
+})
 
-
-app.get('/', function(req, res) {
-  console.log("index");
-  return res.render('index');
-});
+app.get('/', (req, res) => {
+  return res.render('index')
+})
 
 // Handles the route for echo apis
-app.get('/api/remocon', function(req, res) {
-  console.log("received echo request");
-  sendCommand(req.query.command);
-  return res.status(200).end();
-});
+app.get('/api/remocon', (req, res) => {
+  console.log(req.query.commands)
+  const commands = req.query.commands.split(',')
+  commands.forEach(async command => {
+    sendCommand(command)
+    await sleep(200)
+  })
+  return res.status(200).end()
+})
